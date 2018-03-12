9547d691661e78e43129450d53d0abbec6e129e9
magerrrr <kiruha24@mail.ru>
2018-03-12 17:14:05 +0300

    update Index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let baseSimple = [];
+  for (let i = 2; i <= base; i++) {
+    if (base % i === 0) {
+      let simple = {};
+        simple.value = i;
+        simple.simpleNum = 1;
+      
+        base = base / i;
+      while (base % i === 0) {
+        simple.simpleNum++;
+        base = base / i;
+      }
+      baseSimple.push(simple);
+    }
+  }
+  let result = [];
+  
+  baseSimple.forEach(function (item, i, array) {
+    let simpleNumber = factorial(number, item.value);
+    result.push(Math.floor(simpleNumber / item.simpleNum));
+  });
+
+  function factorial(n,k) {
+    let result = 0;
+    while (n) {
+      n = Math.floor(n / k);
+      result += n;
+    }
+    return result;
+  }
+
+  return result.sort(function (a, b) {
+    return a - b;
+  })[0];
 }
